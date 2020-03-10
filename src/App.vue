069c6cd0f54b6a63efcc95f569fd5dc5645b1e32
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-03-10 22:51:51 +0200

    add version 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <NavBar/>
+    <Content/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import NavBar from "./components/TheNavigationBar.vue";
+import Content from "./components/TheHomepageContent.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    NavBar,
+    Content,
   },
 
   data: () => ({
@@ -54,3 +22,7 @@ export default {
   })
 };
 </script>
+
+<style scoped>
+
+</style>
