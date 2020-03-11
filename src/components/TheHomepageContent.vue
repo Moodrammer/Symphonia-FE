163930ebb5ed501fa307c4179da2544108747c87
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-03-11 14:26:40 +0200

    Add: responsive on tablet devices 960px > < 1264px

--- src/components/TheHomepageContent.vue
+++ src/components/TheHomepageContent.vue
@@ -1,33 +1,30 @@
 <template>
-    <v-content class="hero-home-bg-cover">
-    </v-content>
+  <v-content class="hero-home-bg-cover"> </v-content>
 </template>
 
 <script>
 export default {
-    name: "HomepageContent",
-}
+  name: "HomepageContent"
+};
 </script>
 
 <style scoped>
-
 .hero-home-bg-cover {
--webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-font-size: 16px;
-line-height: 1.5;
-font-weight: 400;
-font-family: Circular, Helvetica, Arial, sans-serif, Ruble;
--webkit-box-direction: normal;
-color: #FFF;
-box-sizing: border-box;
-position: absolute;
-left: 50%;
-width: 1610px;
-height: 1523px;
-background: url("http://localhost:8080/hero-burst.svg") no-repeat;
-background-position: top -410px center;
-transform: translate(-50%, 0%);
-background-color: currentColor;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  font-size: 16px;
+  line-height: 1.5;
+  font-weight: 400;
+  font-family: Circular, Helvetica, Arial, sans-serif, Ruble;
+  -webkit-box-direction: normal;
+  color: #fff;
+  box-sizing: border-box;
+  position: absolute;
+  left: 50%;
+  width: 1610px;
+  height: 1523px;
+  background: url("http://localhost:8080/hero-burst.svg") no-repeat;
+  background-position: top -410px center;
+  transform: translate(-50%, 0%);
+  background-color: currentColor;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [
-];
+const routes = [];
 
 const router = new VueRouter({
   mode: "history",
