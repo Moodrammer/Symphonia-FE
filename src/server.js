3d1a5e04ec66710a754eacb11eb5a7460e73740e
Moodrammer <mahmou.m.gody@gmail.com>
2020-03-13 05:39:19 +0200

    Add Mirage js

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import { Server } from "miragejs"
+
+//The makeserver function to be used to enable Mirage to intercept your requests
+export function makeServer({ environment = "development" } = {}) {
+  let server = new Server({
+    environment,
+
+    //Leaving the user model as a reference example to be removed later
+
+    //models: {
+      //user: Model,
+    //},
+
+    //seeds(server) {
+      //server.create("user", { name: "Bob" })
+      //server.create("user", { name: "Alice" })
+    //},
+
+    //routes() {
+      //this.urlPrefix = 'https://api.edamam.com';
+       
+
+      //this.get("/search", schema => {
+        
+        //return schema.users.all()
+         
+      
+      //})
+    //},
+  })
+
+  return server
+}
