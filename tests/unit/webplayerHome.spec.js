3a7620975190950f4c44c3c04dfe845df705b0cb
EmanOthman21 <eothman21@gmail.com>
2020-03-17 23:32:13 +0200

    Add unit testing

---
+++ tests/unit/webplayerHome.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from "@vue/test-utils";
+import Vue from 'vue';
+import vuetify from "vuetify"
+
+import WebplayerHome from "@/views/WebPlayerHome.vue";
+import NavBar from "@/components/WebNavBar.vue"
+import NavDrawer from "@/components/WebNavDrawer.vue"
+
+
+describe("WebplayerHome", () => {
+  let wrapper;
+
+  beforeEach(()=>{
+    Vue.use(vuetify);
+    wrapper = shallowMount(WebplayerHome);
+  })
+
+  it("renders a vue instance", () => {
+    expect(shallowMount(WebplayerHome).isVueInstance()).toBe(true);
+  });
+
+  it("Has Navagation Bar", () =>{
+    expect(wrapper.contains(NavBar)).toBe(true);
+  })
+
+  it("Has Navagation Drawer", () =>{
+    expect(wrapper.contains(NavDrawer)).toBe(true);
+  })
+
+  it("Contains recently played h1", () =>{
+    expect(wrapper.html()).toContain('<h1 class="mb-3">Recently played</h1>')
+  })
+
+  it("Hover data is false", () =>{
+    expect(wrapper.vm.hover).toBe(false);
+  })
+});
