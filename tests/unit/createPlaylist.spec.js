f08317816cd343d0c40640b13f1ac0a3acd9cd69
EmanOthman21 <eothman21@gmail.com>
2020-04-03 10:50:52 +0200

    Add create playlist unit testing

--- tests/unit/createPlaylist.spec.js
+++ tests/unit/createPlaylist.spec.js
@@ -1,27 +1,63 @@
-import { shallowMount } from "@vue/test-utils";
-import Vue from 'vue';
-import vuetify from "vuetify"
+import { mount } from "@vue/test-utils";
+import Vue from "vue"
+import Vuetify from "vuetify";
 
 import CreatePlaylist from "@/components/CreatePlaylist.vue";
 
+
+
+
 describe("CreatePlaylist", () => {
   let wrapper;
+  let vuetify;
 
   beforeEach(()=>{
+    vuetify = new Vuetify()
     Vue.use(vuetify);
-    wrapper = shallowMount(CreatePlaylist);
+    wrapper = mount(CreatePlaylist, {
+      vuetify
+    });
   })
 
   it("renders a vue instance", () => {
-    expect(shallowMount(CreatePlaylist).isVueInstance()).toBe(true);
+    expect(wrapper.isVueInstance()).toBe(true);
   });
 
   it("Contains text feild", () =>{
-    expect(wrapper.html()).toContain('<v-text-field')
+    const textField=wrapper.find("#playlistName");
+    expect(textField.exists()).toBe(true);
   });
 
   it("Empty playlist name at the begining", ()=>{
     expect(wrapper.vm.name).toBe("");
-  })
+  });
 
+  it("Contains cancel button" , () => {
+    const cancelBtn=wrapper.find("#cancel");
+    expect(cancelBtn.text() == "Cancel").toBe(true); 
+  });
+
+  it("Contains create button", () => {
+    const createBtn=wrapper.find("#create");
+    expect(createBtn.text()=="Create").toBe(true);
+  });
+
+  it("Contains close icon" , () => {
+    const icon=wrapper.find("#closeIcon");
+    expect(icon.exists()).toBe(true);
+  });
+
+  it("Input the playlist name",async () => {
+    const textField=wrapper.find("#playlistName");
+    textField.element.value="New Symphonia Playlist";
+    textField.trigger("input");
+    expect(wrapper.vm.name).toBe("New Symphonia Playlist");
+  });
+
+  it("Close the pop up with icon click",async () => {
+    wrapper.vm.dialog=true;
+    const icon=wrapper.find("#closeIcon");
+    icon.trigger("click");
+    expect(wrapper.vm.dialog).toBe(false);
+  });
 });
