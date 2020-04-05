b599197f0eb244e46f0292bbec032a6a29910dfa
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-04-05 03:53:19 +0200

    add: sign up/log in snackbar.

--- src/components/TheSoundPlayer/TheSoundPlayerLogout.vue
+++ src/components/TheSoundPlayer/TheSoundPlayerLogout.vue
@@ -1,17 +1,12 @@
 <template>
   <v-footer app class="sound-player">
     <v-row>
-      <v-col cols="4">
-      </v-col>
+      <v-col cols="4"> </v-col>
 
       <v-col cols="5">
         <div class="audio-controls">
           <!-- shuffle -->
-          <a
-            @click="signIn()"
-            title="shuffle"
-            style="margin-right: 20px;"
-          >
+          <a @click="signIn()" title="shuffle" style="margin-right: 20px;">
             <v-icon small class="icons">
               mdi-shuffle-variant
             </v-icon>
@@ -24,10 +19,7 @@
             </v-icon>
           </a>
           <!-- Start and Pause -->
-          <a
-            @click="signIn()"
-            style="width: 36px; height: 36px;"
-          >
+          <a @click="signIn()" style="width: 36px; height: 36px;">
             <v-icon large class="icons" title="play">
               mdi-play-circle-outline
             </v-icon>
@@ -40,11 +32,7 @@
             </v-icon>
           </a>
 
-          <a
-            @click="signIn()"
-            title="Enable repeat"
-            style="margin-left: 20px;"
-          >
+          <a @click="signIn()" title="Enable repeat" style="margin-left: 20px;">
             <v-icon small class="icons">
               mdi-repeat
             </v-icon>
@@ -53,35 +41,55 @@
 
         <v-toolbar flat color="rgba(0,0,0,0)" height="10">
           <!-- progress bar -->
-          <span class="time" style="padding-right: 10px; margin-top:0px;">00:00</span>
+          <span class="time" style="padding-right: 10px; margin-top:0px;"
+            >00:00</span
+          >
 
-          <input
-            type="range"
-            min="0"
-            max="0"
-            value="0"
-            @click="signIn()"
-          />
+          <input type="range" min="0" max="0" value="0" @click="signIn()" />
 
           <!-- time -->
-          <span class="time" style="padding-left: 10px; margin-top:0px;">00:00</span>
+          <span class="time" style="padding-left: 10px; margin-top:0px;"
+            >00:00</span
+          >
         </v-toolbar>
       </v-col>
 
       <v-spacer></v-spacer>
       <v-col cols="2" style="background: rgba(0, 0, 0, 0);"></v-col>
     </v-row>
+
+    <v-snackbar v-model="snackbar" style="bottom: 100px;">
+      <span>Start listening with a free Spotify account</span> 
+
+      <router-link to="/signup" style="text-decoration: none;">
+      <v-btn color="green" text>
+        sign up
+      </v-btn>
+      </router-link>
+      
+      <router-link to="/login" style="text-decoration: none;">
+      <v-btn color="cyan" text min-width="20">
+        log in
+      </v-btn>
+      </router-link>
+    </v-snackbar>
+
   </v-footer>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      snackbar: false
+    }
+  },
   methods: {
     signIn: function() {
-      console.log("sign in please")
-    }
-  }
-}
+      this.snackbar = true;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
