cc58b49e528e7adc58ff4767228fa409ce900bfb
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-04-17 05:57:58 +0200

    test: audio tag handlers

--- tests/unit/soundplayer.spec.js
+++ tests/unit/soundplayer.spec.js
@@ -51,7 +51,8 @@ describe("TheSoundplayer", () => {
           },
           mutations: {
             setAudio: (state, audio) => {state.audio = audio},
-            setLiked: (state, liked) => {state.liked = liked}
+            setLiked: (state, liked) => {state.liked = liked},
+            setTotalDuration: (state, totalDuration) => {state.totalDuration = totalDuration}
           }
         },
         playlist: {
@@ -59,14 +60,19 @@ describe("TheSoundplayer", () => {
 
           state:{
             audio: "",
-            isQueueOpened: false
+            isQueueOpened: false,
+            setIsFirstSong: false,
+            paused: true
           },
 
           actions: {
             createPlaylist: jest.fn()
           },
           mutations: {
-            setAudio: (state, audio) => {state.audio = audio} 
+            setAudio: (state, audio) => {state.audio = audio},
+            setPaused: (state, paused) => {state.paused = paused},
+            setIsSongLoaded: (state, loaded) => {state.loaded = loaded},
+            setIsFirstSong: (state, isFirstSong) => {state.isFirstSong = isFirstSong}
           },
           getters: {
             audio: (state) => {
@@ -119,6 +125,33 @@ describe("TheSoundplayer", () => {
     wrapper.vm.updateVolume()
     expect(wrapper.vm.isMuted).toEqual(false)
   });
+  
+  //shuffle
+  it("shuffle enabled", () => {
+    wrapper.vm.enableShuffle();
+    expect(wrapper.vm.isShuffleEnabled).toBe(true);
+  })
+
+  it("shuffle disabled", () => {
+    wrapper.vm.disableShuffle();
+    expect(wrapper.vm.isShuffleEnabled).toBe(false);
+  })
+
+  it("repeat enabled", () => {
+    wrapper.vm.enableRepeat();
+    expect(wrapper.vm.isRepeatEnabled).toBe(true);
+  })
+
+  it("RepeatOnce enabled", () => {
+    wrapper.vm.enableRepeatOnce();
+    expect(wrapper.vm.isRepeatEnabled).toBe(false);
+    expect(wrapper.vm.isRepeatOnceEnabled).toBe(true);
+  })
+
+  it("disableRepeatOnce", () => {
+    wrapper.vm.disableRepeatOnce();
+    expect(wrapper.vm.isRepeatOnceEnabled).toBe(false);
+  })
 
   //mute
   it("mute the volume", () => {
@@ -130,6 +163,50 @@ describe("TheSoundplayer", () => {
     expect(wrapper.vm.isMuted).toBe(false)
   })
 
-  
+  it("Loaded handler", () => {
+    wrapper.vm.setAudio({
+      readyState: 3,
+      duration: 3000,
+      play: function() {
+      }
+    })
 
+    wrapper.vm.setIsFirstSong(false);
+    wrapper.vm._handleLoaded();
+
+    expect(wrapper.vm.totalDuration).toEqual(3000);
+    expect(wrapper.vm.isBuffering).toEqual(true);
+  })
+
+  it("playing handler", () => {
+    wrapper.vm.setAudio({
+      currentTime: 3000
+    })
+    wrapper.vm.currentTimeInSec = 10;
+
+    //scenario 1
+    wrapper.vm.isProgressBarPressed = true;
+    wrapper.vm._handlePlayingUI();
+    expect(wrapper.vm.currentTimeInSec).toEqual(10);
+
+    //scenario 2
+    wrapper.vm.isProgressBarPressed = false;
+    wrapper.vm._handlePlayingUI();
+    expect(wrapper.vm.currentTimeInSec).toEqual(3000);
+  })
+
+  it("pause handler", () => {
+    wrapper.vm._handlePause();
+    expect(wrapper.vm.paused).toEqual(true);
+  })
+
+  it("started buffering", () => {
+    wrapper.vm._handlerWaiting();
+    expect(wrapper.vm.isBuffering).toEqual(false);
+  })
+
+  it("buffering is finished", () => {
+    wrapper.vm._handlePlayingAfterBuffering();
+    expect(wrapper.vm.isBuffering).toEqual(true);
+  })
 });
