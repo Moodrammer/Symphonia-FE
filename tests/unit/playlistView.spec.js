ed586cc026db7cfeb9ec5c478184b018647bb6e5
EmanOthman21 <eothman21@gmail.com>
2020-05-28 03:25:07 +0200

    finish playlist view unit testing

--- tests/unit/playlistView.spec.js
+++ tests/unit/playlistView.spec.js
@@ -20,12 +20,6 @@ describe("Playlist View", () => {
     Vue.use(VueRouter);
     Vue.use(Vuex);
 
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn()
-    };
-
     //Mocking the store
     store = new Vuex.Store({
       modules: {
@@ -87,7 +81,7 @@ describe("Playlist View", () => {
           id: "1234"
         }
       },
-      localStorageMock
+      //localStorageMock
     });
   });
 
@@ -171,4 +165,17 @@ describe("Playlist View", () => {
     expect("getPlaylistTracks").toHaveBeenCalled;
     expect("checkFollowed").toHaveBeenCalled;
   });
+
+  it("Open ads popup after route changing",()=>{
+    Storage.prototype.getItem = jest.fn(() => 'bla');
+    wrapper.vm.$options.watch.playlistID.call(wrapper.vm);
+    expect("changeAdsPopup").toHaveBeenCalled
+    expect(wrapper.vm.isLoggedIn()).toBe(true)
+  });
+
+  it("Open ads popup if the user logged in",()=>{
+    Storage.prototype.getItem = jest.fn(() => 'bla');
+    expect("changeAdsPopup").toHaveBeenCalled
+  });
+
 });
