95afa0afe13db006c8ce6efcba46342c40c9a110
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-05-29 20:32:35 +0200

    fix: the current track in queue has wrong style.

--- src/views/TheQueue.vue
+++ src/views/TheQueue.vue
@@ -14,6 +14,7 @@
             :artistName="curTrkArtistName"
             :albumName="albumName"
             :songDuration="trackTotalDurationMs"
+            :ID="curTrkId"
           />
         </v-list>
       </v-col>
@@ -71,6 +72,7 @@ export default {
     ...mapState({
       curTrkName: state => state.track.trackName,
       curTrkArtistName: state => state.track.trackArtistName,
+      curTrkId: state => state.track.trackId,
       queueNextTracks: state => state.track.queueNextTracks,
       trackTotalDurationMs: state => state.track.trackTotalDurationMs,
       albumName: state => state.track.trackAlbumName,
